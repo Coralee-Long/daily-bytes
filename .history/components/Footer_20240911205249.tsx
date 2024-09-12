a1535ae9034a4262567efb2eb6ddc099c02668b5
Coralee <coralee.long.partner@decathlon.com>
2024-09-12 10:56:36 +0200

    project setup

---
+++ .history/components/Footer_20240911205249.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-blue-600 text-white p-4 h-[10vh] mt-auto">
+      <div className="text-center">
+        <p>&copy; 2024 My Portfolio. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
