8016d3af34b38b9e3a06da15234be99f63f0a326
Coralee <coralee.long.partner@decathlon.com>
2024-09-16 08:47:03 +0200

    add more styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,6 +33,49 @@ const config: Config = {
         'white-600': '#a3a3ad',
         'white-800': '#848496',
       },
+      fontSize: {
+        xs: '1.2rem', // 12px
+        sm: '1.4rem', // 14px
+        base: '1.6rem', // 16px (default size)
+        lg: '1.8rem', // 18px
+        xl: '2.0rem', // 20px
+        '2xl': '2.4rem', // 24px
+        '3xl': '3.0rem', // 30px
+        '4xl': '3.6rem', // 36px
+        '5xl': '4.8rem', // 48px
+        '6xl': '6.0rem', // 60px
+      },
+      fontWeight: {
+        thin: '100',
+        extraLight: '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semiBold: '600',
+        bold: '700',
+        extraBold: '800',
+        black: '900',
+      },
+      spacing: {
+        '0': '0px',
+        '1': '0.4rem', // 4px
+        '2': '0.8rem', // 8px
+        '3': '1.2rem', // 12px
+        '4': '1.6rem', // 16px
+        '5': '2.0rem', // 20px
+        '6': '2.4rem', // 24px
+        '8': '3.2rem', // 32px
+        '10': '4.0rem', // 40px
+        '12': '4.8rem', // 48px
+        '16': '6.4rem', // 64px
+        '20': '8.0rem', // 80px
+        '24': '9.6rem', // 96px
+        '32': '12.8rem', // 128px
+        '40': '16.0rem', // 160px
+        '48': '19.2rem', // 192px
+        '56': '22.4rem', // 224px
+        '64': '25.6rem', // 256px
+      },
     },
   },
   plugins: [],
